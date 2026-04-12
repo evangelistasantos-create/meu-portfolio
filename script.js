fa4346a6542e0d368598543747a0cfba4a4fd3ed
evangelistasantos-create <evangelista.santos@professor.edu.pi.gov.br>
2026-04-12 19:31:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 const projetos = [
-    {
-        nome: "Projeto 1",
-        descricao: "Meu primeiro projeto"
-    },
-    {
-        nome: "Projeto 2",
-        descricao: "Outro projeto legal"
-    }
+  { nome: "Portfólio", descricao: "Meu primeiro site feito com HTML, CSS e JavaScript" },
+  { nome: "Site Responsivo", descricao: "Projeto adaptado para celular e computador" },
+  { nome: "Projeto Escolar", descricao: "Atividade prática da disciplina de programação" }
 ];
 
 const lista = document.getElementById("lista-projetos");
 
-projetos.forEach(projeto => {
+if (lista) {
+  projetos.forEach(projeto => {
     const div = document.createElement("div");
     div.classList.add("projeto");
-
     div.innerHTML = `
-        <h3>${projeto.nome}</h3>
-        <p>${projeto.descricao}</p>
+      <h3>${projeto.nome}</h3>
+      <p>${projeto.descricao}</p>
     `;
-
     lista.appendChild(div);
-});
+  });
+} else {
+  console.error("Elemento #lista-projetos não encontrado!");
+}
