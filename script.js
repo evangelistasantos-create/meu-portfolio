ecadbcbea48533ac0869988f0865154992750a22
evangelistasantos-create <evangelista.santos@professor.edu.pi.gov.br>
2026-04-13 19:31:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const projetos = [
-  { nome: "Portfólio", descricao: "Meu primeiro site feito com HTML, CSS e JavaScript" },
-  { nome: "Site Responsivo", descricao: "Projeto adaptado para celular e computador" },
-  { nome: "Projeto Escolar", descricao: "Atividade prática da disciplina de programação" }
+  { nome: "Portfólio", descricao: "Meu primeiro site" },
+  { nome: "Site Responsivo", descricao: "Funciona no celular" },
+  { nome: "Projeto Escolar", descricao: "Atividade da escola" }
 ];
 
 const lista = document.getElementById("lista-projetos");
 
-if (lista) {
-  projetos.forEach(projeto => {
-    const div = document.createElement("div");
-    div.classList.add("projeto");
-    div.innerHTML = `
-      <h3>${projeto.nome}</h3>
-      <p>${projeto.descricao}</p>
-    `;
-    lista.appendChild(div);
-  });
-} else {
-  console.error("Elemento #lista-projetos não encontrado!");
-}
+projetos.forEach(projeto => {
+  const div = document.createElement("div");
+  div.classList.add("card");
+
+  div.innerHTML = `
+    <h3>${projeto.nome}</h3>
+    <p>${projeto.descricao}</p>
+  `;
+
+  lista.appendChild(div);
+});
